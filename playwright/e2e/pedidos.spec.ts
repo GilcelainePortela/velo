1afb6cbb1e136491b59e577d1d88528952789da3
Gilce <gilce_20@hotmail.com>
2026-05-31 23:35:40 -0300

    Desafio: Code Challenge: Você consegue testar sem Test-ID

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -10,14 +10,23 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-Q2BENO')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-Q2BENO')
+  // await page.getByTestId('search-order-button').click()
+  //await page.locator('//button[text()="Buscar Pedido"]').click()
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-Q2BENO')
+  // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
+  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-Q2BENO')
+  await expect(page.locator('div').filter({ hasText: /^PedidoVLO-Q2BENO$/ }).first()).toBeVisible()
+  await expect(page.getByTestId('order-result-VLO-Q2BENO')).toContainText('VLO-Q2BENO')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+  // await expect(page.getByTestId('order-result-status')).toBeVisible()
+  // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByTestId('order-result-VLO-Q2BENO')).toContainText('APROVADO')
+
 
 })
